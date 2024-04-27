f750d1d13b51e88d2f4ca75a9b126ac5b0ad7148
jl <jlonmyway@gmail.com>
2024-04-27 23:51:48 +0800

    mqtt5 publish varibles

--- pkg/mqtt/publish.go
+++ pkg/mqtt/publish.go
@@ -1,10 +1,18 @@
 package mqtt
 
 type Publish struct {
-	Dup       bool
-	Qos       QoS
-	Retain    bool
-	Topic     string
-	MessageID uint16
-	Payload   []byte
+	Dup                    bool
+	Qos                    QoS
+	Retain                 bool
+	Topic                  string
+	PacketID               uint16
+	Payload                []byte
+	PayloadFormatIndicator bool   // from mqtt5
+	MessageExpiryInterval  uint16 // from mqtt5 (seconds)
+	TopicAlias             uint16 // from mqtt5
+	ResponseTopic          string // from mqtt5
+	CorrelationData        []byte // from mqtt5
+	UserProperties         UserProperties
+	SubscriptionIdentifier uint32
+	ContentType            string
 }
