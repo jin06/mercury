a4bdf68e6aa3873853e530edada053bf1d1f3aad
jinlong <jlonmyway@gmail.com>
2025-02-18 23:46:55 +0800

    change closing to stopping

--- internal/server/clients/generic.go
+++ internal/server/clients/generic.go
@@ -19,7 +19,7 @@ func NewClient(handler server.Server, conn net.Conn) *generic {
 		handler:   handler,
 		conn:      connections.NewTCP(conn),
 		connected: make(chan struct{}),
-		closing:   make(chan struct{}),
+		stopping:  make(chan struct{}),
 		closed:    make(chan struct{}),
 		options:   Options{},
 	}
@@ -32,9 +32,9 @@ type generic struct {
 	handler   server.Server
 	options   Options
 	connected chan struct{}
-	closing   chan struct{}
+	stopping  chan struct{}
+	stopOnce  sync.Once
 	closed    chan struct{}
-	closeOnce sync.Once
 	err       error // first error that occurs exits the client
 	// packet channels
 	input  chan *mqtt.Packet
@@ -50,12 +50,11 @@ func (c *generic) ClientID() string {
 
 func (c *generic) Run(ctx context.Context) (err error) {
 	defer func() {
-		if r := recover(); r != nil {
-		}
 		if err != nil {
-			logs.Logger.Err(err)
 			c.setError(err)
+			c.stop(err)
 		}
+		c.Close(ctx)
 	}()
 
 	defer close(c.closed)
@@ -63,32 +62,34 @@ func (c *generic) Run(ctx context.Context) (err error) {
 	c.handler.Reg(c)
 
 	var p mqtt.Packet
+
 	if p, err = c.conn.Read(); err != nil {
 		return err
 	}
+
 	if packet, ok := p.(*mqtt.Connect); ok {
 		if err = c.handler.HandleConnect(packet); err != nil {
 			return
 		}
 	} else {
-		err = utils.ErrNotConnectPacket
-		return
+		return utils.ErrNotConnectPacket
 	}
-	c.runloop(ctx)
+	if err = c.runloop(ctx); err != nil {
+		return err
+	}
 	return nil
 }
 
-func (c *generic) runloop(ctx context.Context) {
+func (c *generic) runloop(ctx context.Context) error {
 	wg := sync.WaitGroup{}
 	select {
 	case <-ctx.Done():
-		return
-	case <-c.closing:
-		return
-	case <-c.closed:
-		return
+		return nil
+	case <-c.stopping:
+		return nil
 	}
 	wg.Done()
+	return nil
 }
 
 func (c *generic) readloop(ctx context.Context) error {
@@ -96,7 +97,7 @@ func (c *generic) readloop(ctx context.Context) error {
 		select {
 		case <-ctx.Done():
 			return nil
-		case <-c.closing:
+		case <-c.stopping:
 			return nil
 		default:
 			p, err := c.conn.Read()
@@ -113,7 +114,7 @@ func (c *generic) writeloop(ctx context.Context) error {
 		select {
 		case <-ctx.Done():
 			return nil
-		case <-c.closing:
+		case <-c.stopping:
 			return nil
 		case p := <-c.output:
 			if err := c.Write(*p); err != nil {
@@ -122,37 +123,44 @@ func (c *generic) writeloop(ctx context.Context) error {
 			}
 		}
 	}
-	return nil
 }
 
 func (c *generic) setError(err error) {
 	atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(&c.err)), nil, unsafe.Pointer(&err))
 }
 
-func (c *generic) handlePacket(p mqtt.Packet) {
+// func (c *generic) handlePacket(p mqtt.Packet) {
 
-	switch p.(type) {
-	case *mqtt.Connect:
-		{
-			_ = &mqtt.Connack{
-				FixHeader: &mqtt.FixedHeader{
-					PacketType:      mqtt.CONNACK,
-					Flags:           0,
-					RemainingLength: 2,
-				},
-			}
-		}
-	}
-}
+// 	switch p.(type) {
+// 	case *mqtt.Connect:
+// 		{
+// 			_ = &mqtt.Connack{
+// 				FixHeader: &mqtt.FixedHeader{
+// 					PacketType:      mqtt.CONNACK,
+// 					Flags:           0,
+// 					RemainingLength: 2,
+// 				},
+// 			}
+// 		}
+// 	}
+// }
 
 func (c *generic) Write(p mqtt.Packet) error {
 	return nil
 }
 
-func (c *generic) Close(ctx context.Context) error {
-	var err error
-	c.closeOnce.Do(func() {
-		close(c.closing)
+func (c *generic) stop(err error) {
+	c.stopOnce.Do(func() {
+		c.setError(err)
+		close(c.stopping)
 	})
-	return err
+}
+
+func (c *generic) Close(ctx context.Context) (err error) {
+	if c.err != nil {
+	}
+	if c.conn != nil {
+		err = c.conn.Close()
+	}
+	return
 }
