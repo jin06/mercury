01bfc2f54f383d70f8037cf7dc3c00b94224b81e
jinlong <jlonmyway@gmail.com>
2025-03-08 18:50:42 +0800

    init topic filter

---
+++ internal/server/topic/topic_filter.go
@@ -0,0 +1,72 @@
+package topic
+
+import (
+	"errors"
+	"strings"
+)
+
+type Type byte
+
+const (
+	TypeCommon = 0
+	TypeShare  = 1
+	TypeSystem = 2
+)
+
+func New(rawName string) (*TopicFilter, error) {
+	tf := &TopicFilter{RawName: rawName}
+	if err := tf.valid(); err != nil {
+		return nil, err
+	}
+	if err := tf.init(); err != nil {
+		return nil, err
+	}
+	return tf, nil
+}
+
+type TopicFilter struct {
+	RawName   string
+	TopicName string
+	Type      Type
+	Group     string // share topic group
+}
+
+func (tf *TopicFilter) valid() error {
+	if len(tf.RawName) == 0 {
+		return errors.New("topic name cannot be empty")
+	}
+	if strings.Contains(tf.RawName, "#") && !strings.HasSuffix(tf.RawName, "/#") {
+		return errors.New("invalid use of wildcard '#' in topic name")
+	}
+	if strings.Contains(tf.RawName, "+") {
+		segments := strings.Split(tf.RawName, "/")
+		for _, segment := range segments {
+			if segment == "+" && len(segment) != 1 {
+				return errors.New("invalid use of wildcard '+' in topic name")
+			}
+		}
+	}
+	if strings.Contains(tf.RawName, "//") {
+		return errors.New("topic name cannot contain empty levels")
+	}
+	return nil
+}
+
+func (tf *TopicFilter) init() error {
+	if strings.HasPrefix(tf.RawName, "$share/") {
+		tf.Type = TypeShare
+		parts := strings.SplitN(tf.RawName, "/", 3)
+		if len(parts) < 3 {
+			return errors.New("invalid shared topic format")
+		}
+		tf.Group = parts[1]
+		tf.TopicName = parts[2]
+	} else if strings.HasPrefix(tf.RawName, "$SYS/") {
+		tf.Type = TypeSystem
+		tf.TopicName = tf.RawName
+	} else {
+		tf.Type = TypeCommon
+		tf.TopicName = tf.RawName
+	}
+	return nil
+}
